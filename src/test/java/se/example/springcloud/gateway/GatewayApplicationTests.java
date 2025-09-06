b59505fbe0b5a52887263f7ac398784a599351ce
sumigg <with.sumitaggarwal@gmail.com>
2025-09-06 14:16:45 +0530

    chapter-10 gateway changes

---
+++ src/test/java/se/example/springcloud/gateway/GatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package se.example.springcloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment=RANDOM_PORT, properties = {"eureka.client.enabled=false"})
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
